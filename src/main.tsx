05e3676df5768e401550fa670f76669a2a05eb81
David Young <david.young@reallysimpleapp.com>
2026-02-22 07:24:01 +0000

    Add Vercel Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client';
 import { ClerkProvider } from '@clerk/clerk-react';
+import { Analytics } from '@vercel/analytics/react';
 import './index.css';
 import 'leaflet/dist/leaflet.css';
 import App from './App.tsx';
@@ -21,6 +22,7 @@ createRoot(document.getElementById('root')!).render(
       signUpFallbackRedirectUrl="/app"
     >
       <App />
+      <Analytics />
     </ClerkProvider>
   </StrictMode>,
 );
